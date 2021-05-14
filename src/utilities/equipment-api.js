baff59c1ed4359096d5a71617ad0e7d7ccbd65b3
github_username <lordsnoosh@gmail.com>
2021-05-14 14:25:43 -0700

    adjust

--- src/utilities/equipment-api.js
+++ src/utilities/equipment-api.js
@@ -4,8 +4,6 @@ const BASE_URL = "https://www.dnd5eapi.co/api/equipment";
 
 export function getEquipmentInfo(equipment) {
   return fetch(`${BASE_URL}`)
-  .then(response => response.json())
-  .then(parsedJSON => console.log(parsedJSON.equipment))
   .then((finalRes) => finalRes.json())
   .catch(equipment);
 }
