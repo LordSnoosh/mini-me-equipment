eeaf599b68781e814cc8d093604e835f8001afa4
Dave Curry <lordsnoosh@gmail.com>
2022-09-30 14:54:18 -0700

    updates

--- src/components/OrderDetail/OrderDetail.jsx
+++ src/components/OrderDetail/OrderDetail.jsx
@@ -1,26 +1,25 @@
-import "./OrderDetail.css";
+import './OrderDetail.css'
 
 export default function OrderDetail({ order, handleCheckout }) {
-  if (!order) return null;
+  if (!order) return null
 
   return (
     <>
-      //currently, the equipment from the menu, is compiled into seprate arrays, instead of a single array
       <ul>
         <>
-        <h2>Order</h2>
+          <h2>Order</h2>
           {order.map((item) => {
             return (
               <li>
                 {item.name} || ({item.url})
               </li>
-            );
+            )
           })}
         </>
-        <button className="btn-sm" onClick={handleCheckout}>
+        <button className='btn-sm' onClick={handleCheckout}>
           SEND ORDER
         </button>
       </ul>
     </>
-  );
+  )
 }
